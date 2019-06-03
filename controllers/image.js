411d05dc4cd5079f347c6a536e7fbee4125703ce
Pankaj Goyal <pkjg1807@gmail.com>
2019-06-03 18:49:12 +0530

    Port changed

--- controllers/image.js
+++ controllers/image.js
@@ -5,7 +5,7 @@ const app = new Clarifai.App({
 
 const handleApiCall=(req,res)=>{
 	app.models.predict( 
-		Clarifai.FACE_DETECT_MODEL,req.body.input)
+		"a403429f2ddf4b49b307e318f00e528b",req.body.input)
 	.then(data=>{
 		console.log(data);
 		res.json(data);
