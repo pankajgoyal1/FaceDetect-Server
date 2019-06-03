be50bc87ad1bacb7dc159fd9f5f38d4e3790d23d
Pankaj Goyal <pkjg1807@gmail.com>
2019-06-04 03:26:48 +0530

    Database url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image=require('./controllers/image.js')
 const db =knex({
 	client:'pg',
 	connection:{
-		host:'127.0.0.1',
+		host:'postgresql-globular-13650',
 		password:'',
 		user:'postgres',
 		database:'first'
